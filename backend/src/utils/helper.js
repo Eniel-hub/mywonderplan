54fba7c6b74038b707392b40f516db6dfce939f9
Eniel-hub <78539777+Eniel-hub@users.noreply.github.com>
2023-07-14 06:58:21 +0800

    Update helper.js

--- backend/src/utils/helper.js
+++ backend/src/utils/helper.js
@@ -1,36 +1,32 @@
-const crypto = require('crypto');
+const crypto = require("crypto");
 
 function EmptyOrRows(rows) {
-    if (!rows) {
-      return [];
-    }
-    return rows;
+  if (!rows) {
+    return [];
+  }
+  return rows;
 }
 
-function swap ([first, second]){
-  let temp = first
-  first = second
-  second = temp
-  return([first, second])
+function swap([first, second]) {
+  let temp = first;
+  first = second;
+  second = temp;
+  return [first, second];
 }
 
-function toHash (password, salt){
-  return crypto.pbkdf2Sync(password, salt, 10000, 60, 'sha512').toString('hex');
+function toHash(password, salt) {
+  return crypto.pbkdf2Sync(password, salt, 10000, 60, "sha512").toString("hex");
 }
 
-function GenPassword (password){
-  const salt = crypto.randomBytes(32).toString('hex');
+function GenPassword(password) {
+  const salt = crypto.randomBytes(32).toString("hex");
   const hash = toHash(password, salt);
-  return {salt, hash}
+  return { salt, hash };
 }
 
-function createVerificationCode (date){
-  const salt = 'MyWonderPlanVerification'
-  const hash = toHash(date, salt)
-}
-module.exports  = { 
+module.exports = {
   EmptyOrRows,
   swap,
   toHash,
-  GenPassword
-}
+  GenPassword,
+};
